70f44c032407c56da98f53dc2871ce0e057a4602
vagrant <vagrant@homestead>
2020-12-15 06:16:40 +0000

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,20 +10,11 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
@@ -37,11 +28,6 @@ class User extends Authenticatable
         });
     }
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
@@ -50,5 +36,10 @@ class User extends Authenticatable
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
+
+    public function statuses()
+    {
+        return $this->hasmany(Status::class);
     }
 }
